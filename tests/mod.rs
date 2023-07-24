787363a2f5a34bc7dda0c04ab4848dc7cda7fa23
Jose Daniel Hernandez <jd@nimiq.com>
2023-07-23 23:52:58 -0600

    Add new method for getting an accounts tree chunk
    Add new method for getting an accounts tree chunk with its respective
    test.

--- tests/mod.rs
+++ tests/mod.rs
@@ -44,6 +44,17 @@ mod tests {
     }
 
     #[test]
+    fn get_accounts_tree_chunk() {
+        let client = client();
+        client
+            .get_accounts_tree_chunk(
+                "A9284B441B56E93DE62F557414CC9B850BAD2BD30CF84B013CFE2EF6E11B6DA6",
+                "",
+            )
+            .unwrap();
+    }
+
+    #[test]
     fn get_block_by_hash() {
         let client = client();
         assert_eq!(
